f0658c49ad673143e0149ce59dacff69c1191263
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2020-04-11 23:51:31 +0300

    modify exp3

--- src/shot_detection_experiments.py
+++ src/shot_detection_experiments.py
@@ -2,11 +2,13 @@ from hmmlearn import hmm
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import math
 
 from copy import deepcopy
 from collections import deque
 
 from src.video_operations import save_frames_as_video
+from scipy.spatial import ConvexHull
 
 np.random.seed(42)
 
@@ -90,7 +92,7 @@ def exp2(input_video_path: str, output_video_path: str, verbose=True):
 # takes 2d array of bools, finds connected component of Trues with the largest area and leaves only that component
 # so other component will be False
 # PS. Two pixels are connected iff they have commond side
-def find_largest_area_component_mask(mask, center_ratio_size):
+def find_largest_area_component_mask(mask, center_ratio_size: float = 0.5):
     q = deque()
     n, m = mask.shape
 
@@ -108,6 +110,7 @@ def find_largest_area_component_mask(mask, center_ratio_size):
             component_size = 0
             q.clear()
             q.append((si, sj))
+
             while len(q) > 0:
                 vi, vj = q.pop()
                 if vi < 0 or vi >= n or vj < 0 or vj >= m or color[vi, vj] != 0 or not mask[vi, vj]:
@@ -118,6 +121,7 @@ def find_largest_area_component_mask(mask, center_ratio_size):
                 q.append((vi + 1, vj))
                 q.append((vi, vj - 1))
                 q.append((vi, vj + 1))
+
             if component_size > best_component_size:
                 best_component_size, best_component_color = component_size, current_color
     return color == best_component_color
@@ -126,7 +130,8 @@ def find_largest_area_component_mask(mask, center_ratio_size):
 # takes frame, cuts off 15% of pixels from all sides (left, right, top, bottom),
 # finds largest connected component and removes from mask other components
 # center_ratio_size is the share of the image which will be cropped before finding
-# largest component
+# largest component, finds convex hull
+# as the result of experiment initial video with table layout will be saved
 def exp3(input_video_path: str, output_video_path, center_ratio_size: float = 0.7):
     capture = cv2.VideoCapture(input_video_path)
     fps = int(np.round(capture.get(cv2.CAP_PROP_FPS)))
@@ -138,6 +143,8 @@ def exp3(input_video_path: str, output_video_path, center_ratio_size: float = 0.
 
         if not response:
             break
+
+        print('!!')
 
         frame = frame[:, :, ::-1]
 
@@ -155,10 +162,28 @@ def exp3(input_video_path: str, output_video_path, center_ratio_size: float = 0.
         max_line_gap = 0
         lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, min_line_length, max_line_gap)
 
-        img = np.zeros_like(frame)
+        points = np.array([(0, 0) for _ in range(2 * len(lines[:, 0, :]))])
 
-        for x1, y1, x2, y2 in lines[:, 0, :]:
-            cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        for i, (x1, y1, x2, y2) in enumerate(lines[:, 0, :]):
+            # cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            points[2 * i] = (x1, y1)
+            points[2 * i + 1] = (x2, y2)
+
+        # print(f'{len(points)} points before convex hull. ', end='')
+        convex_hull = ConvexHull(points)
+        hull_size = len(convex_hull.vertices)
+        # print(f'{len(convex_hull.vertices)} points after convex hull')
+
+        img = frame[:, :, ::-1]
+        for i in range(hull_size):
+            x1, y1 = points[convex_hull.vertices[i]]
+            x2, y2 = points[convex_hull.vertices[(i + 1) % hull_size]]
+            cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 4)
+            cv2.circle(img, (x1, y1), 10, (0, 0, 255), 4)
+
+        # plt.imshow(img[:, :, ::-1])
+        # plt.show()
+        # break
 
         imgs.append(img)
 
@@ -170,4 +195,5 @@ if __name__ == '__main__':
     # for i in range(109):
     #     exp1(f'resources/{dir}/raw/video{dir}_{i}.mp4', f'resources/{dir}/exp1/video{dir}_{i}_exp.mp4', verbose=False)
     #     exp2(f'resources/{dir}/raw/video{dir}_{i}.mp4', f'resources/{dir}/exp2/video{dir}_{i}_exp.mp4', verbose=False)
-    exp3('resources/007/raw/video007_22.mp4', 'hah.mp4', 0.3)
+    # exp3('resources/001/raw/video007_22.mp4', 'hah.mp4', 0.5)
+    exp3('resources/001/raw/video001_12.mp4', 'resources/001/exp3/video001_12_exp3.mp4', 0.5)
